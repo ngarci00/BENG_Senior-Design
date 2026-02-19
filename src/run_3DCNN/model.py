4ef84a85d08248a785242bc346a5de913319483d
ngarci00 <ngarci8@gmu.edu>
2026-02-18 19:26:53 -0500

    added model.py

---
+++ src/run_3DCNN/model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+from torchvision.models import r3d_18, R3D_18_Weights
+from config import use_pretrained_model
+
+def build_model():
+    if use_pretrained_model:
+        weights = R3D_18_Weights.DEFAULT
+        model = r3d_18(weights=weights)
+    else:
+        model = r3d_18(weights=None)
+
+    #Replacing the final connected layer to match the number of classes in our dataset
+    in_features = model.fc.in_features
+    model.fc = nn.Linear(in_features, 2)  # Assuming binary classification (e.g., normal vs. abnormal)
+    return model
