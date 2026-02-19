eb56e9d818c8c39098fcc8bd6decfe79e105c031
ngarci00 <ngarci8@gmu.edu>
2026-02-18 19:28:04 -0500

    updated model.py

--- src/run_3DCNN/model.py
+++ src/run_3DCNN/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torchvision.models import r3d_18, R3D_18_Weights
 from config import use_pretrained_model
-
+#Baseline 3D CNN model using ResNet-18 architecture for video classification, adapted for our specific task of classifying normal vs. abnormal videos.
 def build_model():
     if use_pretrained_model:
         weights = R3D_18_Weights.DEFAULT
