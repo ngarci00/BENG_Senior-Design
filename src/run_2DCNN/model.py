20a00278032009b31a661f9d120b178949246eca
ngarci00 <ngarci8@gmu.edu>
2026-02-20 16:50:09 -0500

    started 2DCNN_model

---
+++ src/run_2DCNN/model.py
@@ -0,0 +1,15 @@
+import torch, torch.nn as nn
+from torchvision.models.video import r3d_18, R3D_18_Weights
+from config import use_pretrained_model
+#Baseline 3D CNN model using ResNet-18 architecture for video classification, adapted for our specific task of classifying normal vs. abnormal videos.
+def build_model():
+    if use_pretrained_model:
+        weights = R3D_18_Weights.DEFAULT
+        model = r3d_18(weights=weights)
+    else:
+        model = r3d_18(weights=None)
+
+    #Replacing the final connected layer to match the number of classes in our dataset
+    in_features = model.fc.in_features
+    model.fc = nn.Linear(in_features, 2)  #Assuming binary classification (e.g., normal vs. abnormal)
+    return model
