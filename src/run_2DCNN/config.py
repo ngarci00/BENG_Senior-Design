1e2488827db6c0c50a6493ad18c6705c333d8e81
ngarci00 <ngarci8@gmu.edu>
2026-02-26 00:18:37 -0500

    config.py for 2DCNN

--- src/run_2DCNN/config.py
+++ src/run_2DCNN/config.py
@@ -14,7 +14,7 @@ index_json_path = os.path.join(data_dir, "index_rec.json") #Path to index JSON f
 # index_json_path = os.path.join(data_dir, "index_poly.json") #Path to index JSON file for polygon annotations
 
 # splits_json_path = os.path.join(data_dir, "splits_rec.json") #Path to splits JSON file for train/val splits (all videos)
-splits_json_path = os.path.join(data_dir, "splits_rec_10.json") #<-- Temorary, smaller splits for quick testing (10)
+splits_json_path = os.path.join(data_dir, "splits_rec_10.json") #<-- Temporary, smaller splits for quick testing (10)
 # splits_json_path = os.path.join(data_dir, "splits_poly_10.json") #Path to splits JSON file for train/val splits
 
 #Output directories:
@@ -23,24 +23,26 @@ runs_path = os.path.join("runs","run_2DCNN") #Base directory for training runs
 #Model & Training Parameters:
 kfolds = 3 #Number of folds for cross-validation
 seed = 42 #Random seed for reproducibility
-epochs = 10 #Number of training epochs try: 10,20, 50, 100 HERE <-
-batch_size = 8 #Batch size for training 
+epochs = 20 #Number of training epochs try: 20, 50, 100 HERE <-
+batch_size = 16 #Batch size for training <- 2D CNN is lighter than 3D CNN so we can afford a larger batch size
 learning_rate = 1e-4 #Learning rate for optimizer also refered to as alpha in some contexts (0.0001) <- Need to play with this value, 
 #as 3D CNNs can be sensitive to learning rate. Start with a small value and adjust based on training stability and convergence.
-weight_decay = 1e-2 #Weight decay for regularization
+weight_decay = 1e-4 #Weight decay for regularization
 
-#Data Augmentation Parameters:
-clip_len = 16 #Number of frames in each video clip sample, this should be < or = to the # of frames in the shortest video. 16 is common choice for 3D CNNs
-clips_per_video_train = 5 #Number of training clips to sample per video per epoch. The more clips the longer the training time! <-
-clips_per_video_val = 5 #Number of validation clips to sample per video per epoch. Fewer clips can speed up validation, but may give noisier estimates of val performance.
+#Frame Sampling / Augmentation:
+frames_per_video_train = 16 # Number of frames to sample from each video for training
+frames_per_video_validation = 32 # Frames to sample for validation.
+sample_mode_train = "random" #Mode for sampling frames during training, options: "uniform", "random", "first_n"
+sample_mode_validation = "uniform" #Mode for sampling frames during validation, options: "uniform", "random", "first_n"
 
-resize_hw = (112,112) #HxW to resize frames for model input. This is a common value for 3D CNNs
-use_only_annotated_frames = True
+resize_hw = (224,224) #ImageNet backbones typically use 224x224, we can also try 112x112 for a lighter load!
+use_only_annotated_frames = True #Whether to use only annotated frames for train/val. 
 
 #For early stopping (k-fold) and if acc reaches performance threshold:
 stop_if_val_acc_perfect = True #Helps us save some time, stops once 100% val acc, set to False if you want to train for all epochs regardless of performance.
 perfect_acc_tolerance = 1e-4 #Tolerance for considering validation acc as perfect, this way we avoid with floating points like 0.99999... being considered perfect.
 
 #Model
-use_pretrained_model = False #we aren't using a pretrained model in this case.
+backbone = "resnet18" #Backbone architecture for 2D CNN, options: "resnet18", "resnet34", "resnet50", etc. ResNet-18 is a good starting point for a balance of performance and speed.
+use_pretrained_model = True #Reccommended baseline for 2D CNN, helps w/ convergence and performance
 
